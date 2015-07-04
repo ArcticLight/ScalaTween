360a77a21d5b697e6cf73efd4bdfc880f63d608b
Hawk Weisman <hi@hawkweisman.me>
2015-07-03 22:11:13 -0400

    Something about 'documentation'

--- src/main/scala/me/arcticlight/animations/ScalaTween.scala
+++ src/main/scala/me/arcticlight/animations/ScalaTween.scala
@@ -2,6 +2,13 @@ package me.arcticlight.animations
 
 object ScalaTween {
   trait TweenOps[A <: TweenOps[A]]  {
+
+    // note that + and - used to have [B <: TweenOps[B]] type bounds
+    // and take params of type TweenOps[B]. These type bounds may need
+    // to be re-introduced if implementors of TweenOps need to be able
+    // to prove type stuff about right-hand sides.
+    //    – Hawk, 3 July 2015
+
     /**
      * Scalar multiply (Tween operation) multiplies this value by a fraction and returns the result
      * @param fraction A [[Float]] value between 0 and 1, inclusive
@@ -25,6 +32,8 @@ object ScalaTween {
 
     /**
      * Perform linear interpolation using TweenOps.
+     * @tparam B a type implementing TweenOps
+     * @param other the other TweenOps with which to lerp
      */
     def lerp[B <: TweenOps[B]](other: TweenOps[B], fraction: Float): A
       = this * fraction + other * (1-fraction)
