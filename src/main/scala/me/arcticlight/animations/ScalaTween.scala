eb3aaa742ac71cf2abf494803e7acb780e29c0ee
Hawk Weisman <hi@hawkweisman.me>
2015-07-03 22:01:21 -0400

    Evil typesystem hacking

--- src/main/scala/me/arcticlight/animations/ScalaTween.scala
+++ src/main/scala/me/arcticlight/animations/ScalaTween.scala
@@ -1,38 +1,36 @@
 package me.arcticlight.animations
 
 object ScalaTween {
-  trait TweenOps[T <: TweenOps[T]] {
+  trait TweenOps[A <: TweenOps[A]]  {
     /**
      * Scalar multiply (Tween operation) multiplies this value by a fraction and returns the result
      * @param fraction A [[Float]] value between 0 and 1, inclusive
      * @return a T scaled by multiplying it with the scalar `fraction` amount
      */
-    def *(fraction: Float): T
+    def *(fraction: Float): A
 
     /**
      * Add (Tween operation) adds together this object and the parameter and returns the result
      * @param other Another [[T]] to add to this one
      * @return The result of adding together `this` and `other`
      */
-    def +(other: T): T
+    def +[B <: TweenOps[B]](other: TweenOps[B]): A
 
     /**
      * Subtract (Tween operation) subtracts the parameter from this object and returns the result.
      * @param other Another [[T]] to subtract from this one
      * @return The result of subtracting `other` from `this`
      */
-    def -(other: T): T
+    def -[B <: TweenOps[B]](other: TweenOps[B]): A
 
     /**
      * Perform linear interpolation using TweenOps.
      */
-    def lerp(other: T, fraction: Float): T = {
-      this * fraction + other * (1-fraction)
-    }
+    def lerp[B <: TweenOps[B]](other: TweenOps[B], fraction: Float): A
+      = this * fraction + other * (1-fraction)
 
-    def lease(other: T, fraction: Float, fease: (Float) => Float) = {
-      this + (other - this) * fease(fraction)
-    }
+    def lease[B <: TweenOps[B]](other: TweenOps[B], fraction: Float, fease: (Float) => Float)
+      = this + (other - this) * fease(fraction)
   }
 
   class AnimationTarget[T <: TweenOps[T]](var value: T) {
