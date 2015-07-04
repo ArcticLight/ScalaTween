cdf5d5ae6be73dcbc7d0685804e4743fe7a16c36
Hawk Weisman <hi@hawkweisman.me>
2015-07-04 13:15:19 -0400

    Add documentation explaining implicits Hopefully this is useful for whomever has to maintain my code.

--- src/main/scala/me/arcticlight/animations/ScalaTween.scala
+++ src/main/scala/me/arcticlight/animations/ScalaTween.scala
@@ -34,8 +34,34 @@ object ScalaTween {
     def lease(other: T, fraction: Float, fease: (Float) => Float): T
       = this + (other - this) * fease(fraction)
   }
+
+  /**
+   * Implicitly convert any `TweenOps[T]` back to the underlying `T`.
+   *
+   * This implicit conversion is basically a reminder to the compiler
+   * that any `TweenOps` for some `T <: TweenOps` is also a `T`, which the
+   * compiler can't seem to prove on its' own. Since the type param on
+   * `TweenOps` gets erased at compile-time, this function never actually
+   * gets called.
+   *
+   * Also, note that while IntelliJ thinks this is wrong, the actual Scala
+   * compiler understands it. Probably should disable that warning.
+   *
+   * @param ops an instance of [[TweenOps]]
+   * @tparam T the underlying type on which that [[TweenOps]] operates
+   * @return the given `TweenOps`, but converted so that the compiler
+   *         can understand it as a `T`
+   */
   implicit def unwrapTweenOps[T <: TweenOps[T]](ops: TweenOps[T]): T = ops
-  implicit def detwopsNumeric[A](twops: TwopsyNumeric[A]): A = twops.value
+
+  /**
+   * Unpack a [[TwopsyNumeric]] to get back the underlying [[Numeric]].
+   *
+   * @param ops A [[TwopsyNumeric]]
+   * @tparam A the underlying type `A : Numeric`
+   * @return the underlying value wrapped by `ops`
+   */
+  implicit def detwopsNumeric[A](ops: TwopsyNumeric[A]): A = ops.value
 
   /**
    * Implicitly adds [[TweenOps]][A] to any `A` with [[Numeric]].
@@ -49,7 +75,14 @@ object ScalaTween {
    */
   implicit class TwopsyNumeric[A : Numeric](val value: A)
   extends TweenOps[TwopsyNumeric[A]] {
-
+    // Basically, just wrap all of the underlying numeric value's
+    // pre-existing mathematical operations. I wish this wasn't necessary,
+    // but apparently Scalac is not quite smart enough to construct a type
+    // proof that         +, - :: (Numeric a) => a -> a -> a
+    // is equivalent to   +, - :: (TweenOps a) => a -> a -> a.
+    //
+    // So we have to do it this way.
+    //  – Hawk
     override def *(fraction: Float): TwopsyNumeric[A]
       = value * fraction
     override def +(other: TwopsyNumeric[A]): TwopsyNumeric[A]
