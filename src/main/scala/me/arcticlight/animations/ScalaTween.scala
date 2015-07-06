ecab31c7777a3e1d61ec5e6d94d7804c29a2f721
Hawk Weisman <hi@hawkweisman.me>
2015-07-05 22:27:56 -0400

    Big commented out WIP attempt to open Twops for Fractional

--- src/main/scala/me/arcticlight/animations/ScalaTween.scala
+++ src/main/scala/me/arcticlight/animations/ScalaTween.scala
@@ -102,6 +102,40 @@ object ScalaTween {
      */
     override def add(a: Float, b: Float): Float = a + b
   }
+  // TODO: finish this, requires TweenOps.mult() to take a
+  //Fractional or Numeric as the left scalar.  
+  // class FractionalsAreTweenOpsable[A : Fractional]
+  // extends TweenOps[A] {
+  //   /**
+  //    * Scalar multiply (Tween operation) multiplies this value by a fraction
+  //    * and returns the result
+  //    * @param a The value to multiply
+  //    * @param f A [[Float]] value between 0 and 1, inclusive
+  //    * @return a T scaled by multiplying it with the scalar `fraction` amount
+  //    */
+  //   override def mult(a: A, b: A): A
+  //     = implicitly[Fractional[A]].times(a,b)
+  //
+  //   /**
+  //    * Subtract (Tween operation) subtracts the parameter from this object
+  //    * and returns the result.
+  //    * @param a The first value to work with
+  //    * @param b Another [[Float]] to subtract from this one
+  //    * @return The result of subtracting `b` from `a`
+  //    */
+  //   override def subt(a: A, b: A): A
+  //     = implicitly[Fractional[A]].minus(a,b)
+  //
+  //   /**
+  //    * Add (Tween operation) adds together this object and the parameter and
+  //    * returns the result
+  //    * @param a The first value to work with
+  //    * @param b Another [[Float]] to add to this one
+  //    * @return The result of adding together `this` and `other`
+  //    */
+  //   override def add(a: A, b: A): A
+  //     = implicitly[Fractional[A]].plus(a,b)
+  // }
 
   class AnimationTarget[T : TweenOps](var value: T)
   extends TweenOps[T] {
