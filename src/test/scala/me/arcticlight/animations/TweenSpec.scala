0ade52f5290500599007b13b8ed68e7c2391a146
Max Clive <arcticlight@arcticlight.me>
2016-02-02 16:14:08 -0500

    Integrate with Gradle Test action

--- src/test/scala/me/arcticlight/animations/TweenSpec.scala
+++ src/test/scala/me/arcticlight/animations/TweenSpec.scala
@@ -2,13 +2,16 @@ package me.arcticlight.animations
 
 import me.arcticlight.animations.ScalaTween.{AnimationTarget, Tween}
 import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
 
+@RunWith(classOf[JUnitRunner])
 class TweenSpec extends fixture.FlatSpec with Matchers {
   type FixtureParam = AnimationTarget[Float]
 
   override def withFixture(test: OneArgTest) = {
     val target = new AnimationTarget(0.0f)
-    try test(target)
+    test(target)
   }
 
   "A Tween" should "interpolate a Float from 0->1" in { x =>
