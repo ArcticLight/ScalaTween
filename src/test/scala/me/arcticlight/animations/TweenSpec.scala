6c78562b7c50cbe77f0bd38fe82d75779d8eebec
Max Clive <arcticlight@arcticlight.me>
2016-02-02 15:55:01 -0500

    ScalaTween skeleton

---
+++ src/test/scala/me/arcticlight/animations/TweenSpec.scala
@@ -0,0 +1,17 @@
+package me.arcticlight.animations
+
+import me.arcticlight.animations.ScalaTween.{AnimationTarget, Tween}
+import org.scalatest._
+
+class TweenSpec extends fixture.FlatSpec with Matchers {
+  type FixtureParam = AnimationTarget[Float]
+
+  override def withFixture(test: OneArgTest) = {
+    val target = new AnimationTarget(0.0f)
+    try test(target)
+  }
+
+  "A Tween" should "interpolate a Float from 0->1" in { x =>
+    Tween(x, 0f, 1f).seekTo(1) shouldBe 1f
+  }
+}
